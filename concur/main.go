7854c7a59febf9644e0477173b5f76f895af9c22
Dave Stearns <dlsinfo@uw.edu>
2017-05-03 20:56:50 -0700

    lots of comments, for Ena

--- concur/main.go
+++ concur/main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"os"
 	"path"
+	"sort"
 	"strings"
 	"time"
 )
@@ -17,14 +18,15 @@ usage:
 `
 
 func processFile(filePath string, q string, ch chan []string) {
-	//TODO: open the file, scan each line,
-	//do something with the word, and write
-	//the results to the channel
+	//open the file
 	f, err := os.Open(filePath)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	//create a new scanner that reads from the file
 	scanner := bufio.NewScanner(f)
+	//create a new empty slice to hold the matches we find
 	matches := []string{}
 	for scanner.Scan() {
 		//read each word
@@ -35,38 +37,76 @@ func processFile(filePath string, q string, ch chan []string) {
 			matches = append(matches, word)
 		}
 	}
+
+	//close the file
 	f.Close()
+
+	//write the matches slice to the channel
+	//so that the caller can retrieve them
 	ch <- matches
 }
 
+//processDir searches each file in the dirPath
+//for the search string `q`
 func processDir(dirPath string, q string) {
-	//TODO: iterate over the files in the directory
-	//and process each, first in a serial manner,
-	//and then in a concurrent manner
+	//get all of the file names in the requested directory
 	fileinfos, err := ioutil.ReadDir(dirPath)
 	if err != nil {
 		log.Fatal(err)
 	}
+	//make a channel that will receive a slices of
+	//strings, one for each file we will process
+	//these slices will be the matching words
 	ch := make(chan []string, len(fileinfos))
+
+	//for each of the files...
 	for _, fi := range fileinfos {
+		//call processFile on its own goroutine
+		//to see the difference in performance, try
+		//removing the `go` keyword, which will make
+		//the processing happen serially instead of
+		//concurrently
 		go processFile(path.Join(dirPath, fi.Name()), q, ch)
 	}
 
+	//read from the channel once for each file we processed
+	//and combine the results into one overall slice of strings
 	totalMatches := []string{}
+
+	//for each file we processed...
 	for i := 0; i < len(fileinfos); i++ {
+		//read the results from the channel
 		matches := <-ch
+		//append the matches to our overall slice
+		//the `...` following the `matches` slice will
+		//convert the slice elements into separate arguments
+		//to the append() function
 		totalMatches = append(totalMatches, matches...)
 	}
+
+	//sort the overall matches slice
+	sort.Strings(totalMatches)
+
+	//print all the matches to the terminal as a comma-delimeted list
 	fmt.Println(strings.Join(totalMatches, ", "))
 }
 
 func main() {
+	//this program will find all words that contain
+	//a particular run of characters supplied by the user
+	//the first argument is the directory containing the
+	//data files, and the second argument is the run of
+	//characters the user wants to search for
 	if len(os.Args) < 3 {
 		fmt.Println(usage)
 		os.Exit(1)
 	}
 
+	//the directory containing the data files
 	dir := os.Args[1]
+
+	//the query the user wants to search for
+	//which will be a run of letters
 	q := os.Args[2]
 
 	fmt.Printf("processing directory %s...\n", dir)
--- crawler/main.go
+++ crawler/main.go
@@ -11,17 +11,52 @@ usage:
 	crawler <starting-url>
 `
 
+//worker is the worker function for our crawler. The main
+//goroutine will start N of these running concurrently.
+//The worker will read the next link out of the `linkq`,
+//call getPageLinks to fetch and parse it, and then write
+//the slice of links in that page to the `resultsq`
 func worker(linkq chan string, resultsq chan []string) {
+	//range over the linkq
+	//as noted below, channel support the range
+	//operator, returning the next item in the channel
+	//(in this case, a link to fetch and parse)
 	for link := range linkq {
+		//call getPageLinks() to fetch the page, parse it,
+		//and extract all of the hyperlinks in that page
 		plinks, err := getPageLinks(link)
+		//if we get an error, report it, but continue
+		//so that the worker just gets the next item from
+		//the `linkq`
 		if err != nil {
 			fmt.Printf("ERROR fetching %s: %v\n", link, err)
 			continue
 		}
 
+		//write a message saying which link we fetched, and
+		//how many links we found in that page
 		fmt.Printf("%s (%d links)\n", link, len(plinks.Links))
+
+		//sleep for half a second so that we don't pelt the
+		//server with too many concurrent requests
 		time.Sleep(time.Millisecond * 500)
+
+		//if we found some links in the fetched page...
 		if len(plinks.Links) > 0 {
+			//use a new goroutine to write the links
+			//to the resultq
+			//we use a new goroutine here so that the
+			//worker goroutine never gets blocked while
+			//trying to write to the resultsq. Since the
+			//main goroutine reads from that same queue
+			//and writes each link to the `linkq`, which
+			//the worker goroutine reads from, we could get
+			//into a deadlock where each goroutine is waiting
+			//for the other to write or read something.
+			//using a new goroutine ensures the worker is
+			//never blocked, so it can always read the next
+			//link off of the linkq and thereby free-up the
+			//main goroutine to read more off of the resultsq
 			go func(links []string) {
 				resultsq <- links
 			}(plinks.Links)
@@ -35,20 +70,54 @@ func main() {
 		os.Exit(1)
 	}
 
+	//number of worker go routines
 	nWorkers := 1000
+
+	//a channel that contains links to fetch and parse.
+	//the second argument specifies how many links can
+	//be written to the channel at a time.
+	//if the channel is full, further writes to the channel
+	//will block until a link is read from the channel
 	linkq := make(chan string, 1000)
+
+	//a channel for reporting results, which are all the
+	//links in a page that was fetched and parsed.
+	//the channel type here is []string, as we may find
+	//multiple links in a given page
 	resultsq := make(chan []string, 1000)
+
+	//start our worker goroutines, passing
+	//the linkq and the resultsq
 	for i := 0; i < nWorkers; i++ {
 		go worker(linkq, resultsq)
 	}
 
+	//write the URL the user supplied on the command
+	//line to the linkq channel so that the first worker
+	//can start crawling
 	linkq <- os.Args[1]
 
+	//construct a map to track links we've already seen
+	//so that we don't fetch them again
 	seen := map[string]bool{}
+
+	//range over the resultsq
+	//channels support the range operator, and will
+	//return the next item in the channel for each
+	//loop iteration
 	for links := range resultsq {
+		//since resultsq is a channel of []string
+		//the `links` variable is a []string
+		//so we can range over it like any slice
 		for _, link := range links {
+			//if we haven't seen this link before
 			if !seen[link] {
+				//mark that we have seen it
 				seen[link] = true
+				//and add it to the linkq,
+				//which is the channel the workers
+				//read from to get the next link
+				//to fetch and parse
 				linkq <- link
 			}
 		}
