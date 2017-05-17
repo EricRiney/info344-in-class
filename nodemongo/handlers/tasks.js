a5fe65a93f73546d917f3b7fd34374a0d553eba9
Dave Stearns <dlsinfo@uw.edu>
2017-05-17 14:22:45 -0700

    midway through class

--- nodemongo/handlers/tasks.js
+++ nodemongo/handlers/tasks.js
@@ -1,11 +1,37 @@
 "use strict";
 
 const express = require('express');
+const Task = require('../models/tasks/task.js');
 
 //export a function from this module 
 //that accepts a tasks store implementation
 module.exports = function(store) {
-    //TODO: create an express.Router()
-    //add handlers to it
-    //and return the configured Router
+    //create a new Mux
+    let router = express.Router();
+
+    router.get('/v1/tasks', async (req, res, next) => {
+        try {
+            let tasks = await store.getAll();
+            res.json(tasks);
+        } catch(err) {
+            next(err);
+        }
+    });
+
+    router.post('/v1/tasks', async (req, res, next) => {
+        try {
+            let task = new Task(req.body);
+            let err = task.validate();
+            if (err) {
+                res.status(400).send(err.message);
+            } else {
+                let result = await store.insert(task);
+                res.json(task);
+            }
+        } catch(err) {
+            next(err);
+        }
+    });
+
+    return router;
 };
